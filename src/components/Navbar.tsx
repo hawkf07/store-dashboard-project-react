12d5e50856f4fe4ac937b81a956cba5b74781ef8
hawkf07 <hawkf07@proton.me>
2023-01-27 14:09:53 +0700

    setup libraries

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <header>
+        <a href="#">
+          <img src="img" alt="Logo Image" /> <p>Pet Central</p>
+        </a>
+        <p>Role Admin</p>
+      </header>
+      <ul>
+        <li>
+          <a href="#">Item 1</a>
+        </li>
+        <li>
+          <a href="#">Item 2</a>
+        </li>
+        <li>
+          <a href="#">Item 3</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export { Navbar };
