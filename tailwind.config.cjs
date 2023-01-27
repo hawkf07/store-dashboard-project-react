61bb40b60fdddcdb003fb24bdd2c746c53ab6ed8
hawkf07 <hawkf07@proton.me>
2023-01-27 15:49:02 +0700

    adding & configuring daisyui

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,ts,tsx,css,html}'],
+  content: ["./src/**/*.{js,ts,tsx,css,html}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+
+  plugins: [require("daisyui")],
+  daisyui:{
+    themes:["light"]
+  }
+};
