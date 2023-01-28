f8e1b7ce2aab51684dd5edd7ce4cf091cea6b78d
hawkf07 <hawkf07@proton.me>
2023-01-28 10:44:41 +0700

    more api functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,26 @@
 import axios from "axios";
+import { useQuery } from "react-query";
 import { ProductRoot } from "../types";
 const fetchAllProduct = async () => {
-  const response = await axios.get("https://dummyjson.com/products");
+  const response = await axios.get("https://dummyjson.com/products?limit=10&skip=0");
   const data: ProductRoot = response.data;
-
   return data;
 };
 
-export { fetchAllProduct };
+const searchProductByQuery = async (q: string) => {
+  const response = await axios.get("https://dummyjson.com/products/search?q=" + q)
+  const data: ProductRoot = response.data
+  console.log(q)
+  return data
+}
+const usePagination = (pageNumber: number) => {
+  return useQuery(["paginatedProduct", pageNumber], async () => {
+    const { data }: { data: ProductRoot } = await axios.get("https://dummyjson.com/products?limit=10&skip=" + pageNumber)
+    return data
+  }, { keepPreviousData: true })
+
+}
+
+export { fetchAllProduct, searchProductByQuery, usePagination };
+
+
