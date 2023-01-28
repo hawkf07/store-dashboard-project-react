3f48ff39a665c591d5df6936cb9106b0d5ac3bc1
hawkf07 <hawkf07@proton.me>
2023-01-28 08:29:32 +0700

    exporting sidebar components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { MainContent, Navbar, Sidebar } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="main bg-gray-200 min-h-screen w-screen">
